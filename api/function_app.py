5d1fc807d87bb038fff8e7a9d20560289f0711ad
shksin <deepshikha16@gmail.com>
2025-01-16 21:14:37 +1100

    test-api

--- api/function_app.py
+++ api/function_app.py
@@ -1,16 +1,5 @@
 import azure.functions as func
-import openai
-from azurefunctions.extensions.http.fastapi import Request, StreamingResponse, JSONResponse
-import asyncio
-import os
 import logging
-import pyodbc
-import requests
-import json
-from datetime import datetime, timedelta
-from bs4 import BeautifulSoup
-from azure.core.credentials import AzureKeyCredential
-from azure.search.documents import SearchClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -37,44 +26,3 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
-
-@app.route(route="get-ice-server-token", methods=[func.HttpMethod.GET, func.HttpMethod.POST])
-def get_ice_server_token(req: Request) -> JSONResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    # Define token endpoint
-    token_endpoint = f"https://{region}.tts.speech.microsoft.com/cognitiveservices/avatar/relay/token/v1"
-
-    # Make HTTP request with subscription key as header
-    response = requests.get(token_endpoint, headers={"Ocp-Apim-Subscription-Key": subscription_key})
-
-    if response.status_code == 200:
-        return JSONResponse(
-            content = response.json(),
-            status_code=200,
-            headers={"Content-Type": "application/json"}
-        )
-    else:
-        return func.HttpResponse(response.status_code)
-    
-
-@app.route(route="get-speech-token", methods=[func.HttpMethod.GET, func.HttpMethod.POST])
-def get_speech_token(req: Request) -> JSONResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    # Define token endpoint
-    token_endpoint = f"https://{region}.api.cognitive.microsoft.com/sts/v1.0/issueToken"
-
-    # Make HTTP request with subscription key as header
-    response = requests.post(token_endpoint, headers={"Ocp-Apim-Subscription-Key": subscription_key})
-
-    print(response)
-
-    if response.status_code == 200:
-        return JSONResponse(
-            content = {"token": response.text},
-            status_code=200,
-            headers={"Content-Type": "application/json"}
-        )
-    else:
-        return func.HttpResponse(response.status_code)
