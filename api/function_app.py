19fe963f35e0cbca41c0edd4788bd4bca1b5b7cf
shksin <deepshikha16@gmail.com>
2025-01-16 21:07:59 +1100

    more changes

--- api/function_app.py
+++ api/function_app.py
@@ -17,7 +17,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 subscription_key = os.getenv("AZURE_SPEECH_API_KEY")
 region = os.getenv("AZURE_SPEECH_REGION")
 
-@app.route(route="get-oai-response")
+@app.route(route="get-oai-response", methods=[func.HttpMethod.GET, func.HttpMethod.POST])
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
